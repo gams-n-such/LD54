ca345db8c0d2a8dd4ca6fdd22e9dbdc7681e5b80
Denis Maximenko <manlok71@gmail.com>
2023-10-02 11:22:23 +0400

    Add countdown timer

---
+++ Gameplay/countdown_timer.gd
@@ -0,0 +1,41 @@
+class_name CountdownTimer
+extends Node
+
+signal tick(seconds_left)
+signal timeout
+
+@export var countdown_seconds : int = 3
+@export var autostart : bool = false
+@export var tick_on_timeout : bool = false
+
+var countdown_counter : int = 1
+
+func _ready():
+	if autostart:
+		start()
+	return
+
+func start():
+	if countdown_seconds <= 0:
+		timeout.emit()
+		return
+	countdown_counter = countdown_seconds
+	begin_next_tick()
+	return
+
+func begin_next_tick():
+	%TickTimer.start()
+	return
+
+func _on_tick_timeout():
+	countdown_counter -= 1
+	
+	if countdown_counter > 0 or tick_on_timeout:
+		tick.emit(countdown_counter)
+	
+	if countdown_counter > 0:
+		begin_next_tick()
+	else:
+		timeout.emit()
+		
+	return
